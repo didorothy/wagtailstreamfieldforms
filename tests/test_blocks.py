e2e937aa7f15367c2a2475659e0b16e86d702d5d
ddorothy <ddorothy@positiveaction.org>
2017-12-20 10:46:24 -0500

    Added tests for MultiLineFormFieldBlock.

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from wagtailstreamfieldforms.blocks import (
     FormFieldBlockMixin,
     SingleLineFormFieldBlock,
+    MultiLineFormFieldBlock,
 )
 
 
@@ -65,4 +66,36 @@ class TestSingleLineFormFieldBlock(TestCase):
         with self.assertRaises(KeyError):
             slffb.get_field_options({})
 
+class TestMultiLineFormFieldBlock(TestCase):
 
+    def test_get_field_options(self):
+        mlffb = MultiLineFormFieldBlock()
+        field_options = {
+            'label': 'Test Label',
+            'help_text': 'There is no help.',
+            'required': True,
+            'default_value': 'Test Value'
+        }
+        opts = mlffb.get_field_options(field_options)
+        self.assertEqual(opts['label'], field_options['label'])
+        self.assertEqual(opts['help_text'], field_options['help_text'])
+        self.assertEqual(opts['required'], field_options['required'])
+        self.assertEqual(opts['initial'], field_options['default_value'])
+
+    def test_get_field_options_bad_options(self):
+        mlffb = MultiLineFormFieldBlock()
+        with self.assertRaises(KeyError):
+            mlffb.get_field_options({})
+
+    def test_create_field(self):
+        mlffb = MultiLineFormFieldBlock()
+        field_options = {
+            'label': 'Test Label',
+            'help_text': 'There is no help.',
+            'required': True,
+            'default_value': 'Test Value'
+        }
+        field = mlffb.create_field(field_options)
+        self.assertIsInstance(field, forms.CharField)
+        self.assertIsInstance(field.widget, forms.Textarea)
+        self.assertEqual(field.initial, field_options['default_value'])
